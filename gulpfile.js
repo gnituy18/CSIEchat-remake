372362efcc668d65f05d88122eef6007d716dcaa
Charlie Hsu <hsu21609@gmail.com>
2017-02-21 14:13:32 +0800

    Modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp')
-var browserify = require('gulp-browserify')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var source = require('vinyl-source-stream')
 var nodemon = require('gulp-nodemon')
 
 gulp.task('build', function () {
-  return gulp.src('./client/main.js')
-  .pipe(browserify({'debug': !gulp.env.production}))
+  return browserify('./client/main.js')
+  .transform(babelify, {'presets': ['es2015']})
+  .bundle()
+  .pipe(source('main.js'))
   .pipe(gulp.dest('./public/js'))
 })
 
@@ -12,7 +16,8 @@ gulp.task('watch', ['build'], function () {
   gulp.watch('./client/**/*.js', ['build'])
   nodemon({
     'script': 'server/server.js',
-    'ignore': [ 'views/', 'client/', 'public/', 'gulpfile.js' ]
+    'ignore': [ 'views/', 'client/', 'public/', 'gulpfile.js' ],
+    'env': {'PORT': '3000'}
   })
 })
 
